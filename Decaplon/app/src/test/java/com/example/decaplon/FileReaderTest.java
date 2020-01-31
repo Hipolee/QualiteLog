8a11da3aaa8eec68ee4cbaaa17368f792fd05957
Hipolee <lucas.bourzier@etu.uca.fr>
2020-01-31 20:29:16 +0100

    Sauvegarde

---
+++ Decaplon/app/src/test/java/com/example/decaplon/FileReaderTest.java
@@ -0,0 +1,31 @@
+package com.example.decaplon;
+
+import com.example.decaplon.Model.ContentItemShop;
+import com.example.decaplon.Utils.XmlParser;
+
+import org.junit.Test;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+
+public class FileReaderTest {
+
+    /*@Test
+    public void _isCorrect() {
+        try {
+            String fileName = "C:/Users/user/Desktop/Quallog/Decaplon/app/src/main/java/com/example/decaplon/Save/Data.xml";
+            File file = new File(fileName);
+            FileInputStream is = new FileInputStream(file);
+            assertNotNull(XmlParser.parse(is));
+        }catch (IOException e){
+            e.printStackTrace();
+        }catch (XmlPullParserException xe){
+            xe.printStackTrace();
+        }
+    }*/
+}
